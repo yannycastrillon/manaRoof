f46ca7650b678b46017683c0c0e59a541f0739aa
Yanny Castrillon <yannyandres117@gmail.com>
2018-09-18 23:43:22 -0500

    Refactor request specs to build integration testing. Build employee controller to make the specs successful.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,25 +1,69 @@
 class EmployeesController < ApplicationController
-  before_action :set_employee, except: [:index]
+  before_action :set_employee, except: [:index, :create]
 
   def index
-    render json: Employee.all
+    render json: Employee.all, status: :ok
   end
 
   def show
+    render json: @employee
   end
 
   def create
+    new_employee = Employee.new(employee_params)
+    if new_employee.save
+      render json: new_employee, status: :created
+    else
+      render json: new_employee.errors, status: :unprocessable_entity
+    end
+
   end
 
   def update
+    if @employee
+      if @employee.update_attributes(employee_params)
+        render json: @employee, status: :ok
+      else
+        render @employee.errors, status: :unprocessable_entity
+      end
+    else
+      render json: { errors: [I18n.t('errors.employee.none_found')] },
+                   status: :bad_request
+    end
   end
 
-  def detele
+  def destroy
+    if @employee
+      @employee.destroy
+    end
   end
 
   private
 
   def set_employee
     @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:first_name,
+                                     :last_name,
+                                     :date_of_birth,
+                                     :gender,
+                                     :driver_license,
+                                     :salary_per_hour,
+                                     :ssn,
+                                     :cell_phone,
+                                     :email,
+                                     :street,
+                                     :second_address,
+                                     :city,
+                                     :state,
+                                     :zipcode,
+                                     :nationality,
+                                     :start_date,
+                                     :phone_number,
+                                     :status,
+                                     :employable_type,
+                                     :employable_id)
   end
 end
---
+++ spec/requests/employees_spec.rb
@@ -0,0 +1,118 @@
+require 'rails_helper'
+
+RSpec.describe EmployeesController, type: :request do
+  describe 'get #index' do
+    context 'when user is login' do
+      it 'must list all employee' do
+        get employees_path
+
+        expect(response).to be_successful
+        # expect(parse_body.count).to be >= 1
+      end
+      it 'must response as json format' do
+        get employees_path
+
+        expect(response.content_type).to eq 'application/json'
+      end
+    end
+
+  end
+
+  describe 'get #show' do
+    let(:employee) { create(:employee, :as_manager) }
+
+    context 'when user is login' do
+      it "must bring an employee's details" do
+        get employee_path(id: employee.id)
+        parse_body = JSON.parse(response.body)
+
+        expect(response).to be_successful
+        expect(parse_body["email"]).to_not be_nil
+      end
+      it 'must response as json format' do
+        get employee_path(id: employee.id)
+
+        expect(response.content_type).to eq "application/json"
+      end
+    end
+  end
+
+  describe "post #create" do
+    let(:worker)  { build(:employee, :as_worker) }
+    let(:manager) { build(:employee, :as_manager) }
+
+    context 'when user is login' do
+      context 'when creating a new manager employee' do
+        it 'must persist employee' do
+          expect {
+            post employees_path, params: { employee: manager.attributes }
+          }.to change{ Employee.count }.by(1)
+          manager_record = Manager.find(manager.employable_id)
+
+          expect(manager_record).to be_present
+        end
+
+        it 'must return a head: created' do
+          post employees_path, params: { employee: manager.attributes }
+
+          expect(response).to have_http_status(:created)
+        end
+      end
+
+      context 'when creating a new worker employee' do
+        it 'must persist employee' do
+          expect {
+            post employees_path, params: { employee: worker.attributes }
+          }.to change{ Employee.count }.by(1)
+
+          worker_record = Worker.find(worker.employable_id)
+
+          expect(worker_record).to  be_present
+        end
+        it 'must return a head: created' do
+          post employees_path, params: { employee: worker.attributes }
+
+          expect(response).to have_http_status(:created)
+        end
+      end
+    end
+  end
+
+  describe 'Put #update' do
+    let(:employee) { create(:employee, :as_manager) }
+
+    context 'when user is login' do
+      context 'when updating an existing employee' do
+        it 'must update the fields' do
+          put employee_path(id: employee.id ), params: {
+                                                employee: {
+                                                  first_name: 'Yanny',
+                                                  last_name: 'Castrillon',
+                                                  email: 'yanny.castrillon@example.com'
+                                                }
+                                               }
+
+          emp = Employee.find(employee.id)
+
+          expect(emp.first_name).to eql 'Yanny'
+          expect(emp.last_name).to eql 'Castrillon'
+          expect(emp.email).to eql 'yanny.castrillon@example.com'
+        end
+        it 'must respond success' do
+          put employee_path(id: employee.id), params: {
+                                                employee: {
+                                                  first_name: 'Yanny',
+                                                  last_name: 'Castrillon',
+                                                  email: 'yanny.castrillon@example.com'
+                                                }
+                                               }
+          expect(response).to be_successful
+        end
+      end
+    end
+  end
+
+  describe 'Delete #destroy' do
+
+  end
+end
