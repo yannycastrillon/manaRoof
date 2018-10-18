597b4192478dcf7d1d713c7ef4f5383feba53ee9
Yanny Castrillon <yannyandres117@gmail.com>
2018-10-17 23:24:06 -0500

    Incorporate the sections on the homepage to build up first page. Add homepage team image.

--- app/javascript/packs/bundles/HomePage/home_page.jsx
+++ app/javascript/packs/bundles/HomePage/home_page.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import Footer from './components/footer/main_footer.jsx';
 import Menu from './components/navbar/main_menu.jsx';
 
+// Sections
+import PageHeader from './sections/page_header.jsx';
+import PageSection from './sections/page_section.jsx';
+import HomeTeamSection from './sections/home_team_section.jsx';
+
+//Images
+import HomePageTeam from 'images/home-page/home-team.png';
+
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +19,14 @@ class HomePage extends React.Component {
     return (
       <div className="home-page">
         <Menu isUserlogIn={this.props.isUserlogIn} />
+        <PageHeader>
+        <HomeTeamSection
+          title="Taking care of our Workers."
+          subtitle="ManaRoof brings quality and team opportunities to our workforce."
+          team_image_url={HomePageTeam} />
+        </PageHeader>
+        <PageSection />
+        <PageSection />
         <Footer />
       </div>
     )
