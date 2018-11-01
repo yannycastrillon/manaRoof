18feee1ea2a3e8b0f433e077fa441110ac2c28b4
Yanny Castrillon <yannyandres117@gmail.com>
2018-10-31 23:52:02 -0500

    Finish the yup validation scheme for them employee forms.

--- app/javascript/packs/bundles/Employee/schemas/employee.schema.js
+++ app/javascript/packs/bundles/Employee/schemas/employee.schema.js
@@ -5,11 +5,19 @@ const schema = values =>
     first_name: yup.string().required('Please enter your First Name'),
     last_name: yup.string().required('Please enter your Last Name'),
     gender: yup.string().required('Please choose your gender'),
+    ssn: yup.string().required('Enter social security number'),
     date_of_birth: yup.date().required("Please enter a valid date").default(() => { return new Date }),
     driver_license: yup.string().required('Please enter your Driver License'),
     salary_per_hour: yup.number().positive().integer(),
     cell_phone: yup.string().required('Please enter a cellphone number'),
-    start_date: yup.date().required("Please enter a valid date").default(() => { return new Date })
+    phone_number: yup.string(),
+    street: yup.string().required('Please enter a street address'),
+    second_address: yup.string(),
+    city: yup.string().required('Please select a city'),
+    state: yup.string().required('Please select a state'),
+    zipcode: yup.number().test('len', 'Must be a valid zipcode', val => val.toString().length === 5).required('Please enter your zipcode'),
+    nationality: yup.string(),
+    start_date: yup.date().required("Please enter a valid date starting date").default(() => { return new Date })
   });
 
 export default schema;
